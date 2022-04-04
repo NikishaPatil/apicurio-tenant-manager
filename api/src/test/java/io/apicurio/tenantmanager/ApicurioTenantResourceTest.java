034491c6de28a3349bdf3b451e7b1d7b1ce5de88
Enda Phelan <ephelan@redhat.com>
2022-04-04 16:00:15 +0100

    rename package to 'io.apicurio.tenantmanager'

---
+++ api/src/test/java/io/apicurio/tenantmanager/ApicurioTenantResourceTest.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright 2021 Red Hat
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.apicurio.tenantmanager;
+
+import io.apicurio.tenantmanager.api.datamodel.NewApicurioTenantRequest;
+import io.apicurio.tenantmanager.api.datamodel.ApicurioTenant;
+import io.apicurio.tenantmanager.api.datamodel.ApicurioTenantList;
+import io.apicurio.tenantmanager.api.datamodel.TenantResource;
+import io.apicurio.tenantmanager.api.datamodel.TenantStatusValue;
+import io.apicurio.tenantmanager.api.datamodel.UpdateApicurioTenantRequest;
+import io.apicurio.tenantmanager.client.TenantManagerClientImpl;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+/**
+ * @author Fabian Martinez
+ */
+@QuarkusTest
+public class ApicurioTenantResourceTest {
+
+    private static final String TENANTS_PATH = "/api/v1/tenants";
+
+    @BeforeEach
+    public void cleanup() {
+        var client = new TenantManagerClientImpl("http://localhost:8081/");
+        ApicurioTenantList list = client.listTenants(null, null, null, null, null);
+        list.getItems().forEach(t -> {
+            if (t.getStatus() == TenantStatusValue.READY) {
+                client.deleteTenant(t.getTenantId());
+            }
+        });
+
+        Response res = given()
+          .when().params("status", "READY").get(TENANTS_PATH)
+          .thenReturn();
+
+        assertEquals(200, res.statusCode());
+
+        var search = res.as(ApicurioTenantList.class);
+        assertEquals(0, search.getItems().size());
+        assertEquals(0, search.getCount());
+    }
+
+    @Test
+    public void testCRUD() {
+        NewApicurioTenantRequest req = new NewApicurioTenantRequest();
+        req.setTenantId(UUID.randomUUID().toString());
+        req.setOrganizationId("aaa");
+        req.setName("foo");
+        req.setDescription("bar");
+        TenantResource tr = new TenantResource();
+        tr.setLimit(5L);
+        tr.setType("MAX_TOTAL_SCHEMAS_COUNT");
+        req.setResources(List.of(tr));
+
+        Response res = given()
+            .when()
+            .contentType(ContentType.JSON)
+            .body(req)
+            .post(TENANTS_PATH)
+            .thenReturn();
+
+        assertEquals(201, res.statusCode());
+
+        ApicurioTenant tenant = res.as(ApicurioTenant.class);
+
+        assertNotNull(tenant);
+        assertNotNull(tenant.getTenantId());
+        assertNotNull(tenant.getCreatedOn());
+        assertNotNull(tenant.getResources());
+        assertNotNull(tenant.getName());
+        assertNotNull(tenant.getDescription());
+        assertFalse(tenant.getResources().isEmpty());
+
+        testGetTenant(tenant.getTenantId(), req);
+
+        testUpdateTenant(tenant.getTenantId());
+
+        testDelete(tenant.getTenantId());
+    }
+
+    @Test
+    public void testNotFound() {
+        testTenantNotFound("abcede");
+    }
+
+    private void testGetTenant(String tenantId, NewApicurioTenantRequest req) {
+        Response res = given()
+            .when().get(TENANTS_PATH + "/" + tenantId)
+            .thenReturn();
+
+        assertEquals(200, res.statusCode());
+
+        ApicurioTenant tenant = res.as(ApicurioTenant.class);
+
+        assertEquals(tenantId, tenant.getTenantId());
+        assertEquals(req.getOrganizationId(), tenant.getOrganizationId());
+        assertNotNull(req.getResources());
+        assertNotNull(tenant.getResources());
+        assertEquals(toString(req.getResources()), toString(tenant.getResources()));
+        assertEquals(req.getName(), tenant.getName());
+        assertEquals(req.getDescription(), tenant.getDescription());
+    }
+
+
+    private void testUpdateTenant(String tenantId) {
+        UpdateApicurioTenantRequest req = new UpdateApicurioTenantRequest();
+        req.setDescription("new description");
+        req.setName("new name");
+        TenantResource tr = new TenantResource();
+        tr.setLimit(256L);
+        tr.setType("MAX_LABEL_SIZE_BYTES");
+        req.setResources(List.of(tr));
+
+        given().when()
+                .contentType(ContentType.JSON)
+                .body(req)
+                .put(TENANTS_PATH + "/" + tenantId)
+                .then()
+                .statusCode(204);
+
+        testGetTenantUpdated(tenantId, req);
+    }
+
+    private void testGetTenantUpdated(String tenantId, UpdateApicurioTenantRequest req) {
+        Response res = given()
+            .when().get(TENANTS_PATH + "/" + tenantId)
+            .thenReturn();
+
+        assertEquals(200, res.statusCode());
+
+        ApicurioTenant tenant = res.as(ApicurioTenant.class);
+
+        assertEquals(tenantId, tenant.getTenantId());
+        assertNotNull(req.getResources());
+        assertNotNull(tenant.getResources());
+        assertEquals(req.getName(), tenant.getName());
+        assertEquals(req.getDescription(), tenant.getDescription());
+        assertEquals(toString(req.getResources()), toString(tenant.getResources()));
+    }
+
+    public void testDelete(String tenantId) {
+        given()
+            .when().delete(TENANTS_PATH + "/" + tenantId)
+            .then()
+               .statusCode(204);
+
+        Response res = given()
+                .when().get(TENANTS_PATH + "/" + tenantId)
+                .thenReturn();
+        assertEquals(200, res.statusCode());
+        ApicurioTenant tenant = res.as(ApicurioTenant.class);
+        assertEquals(TenantStatusValue.TO_BE_DELETED, tenant.getStatus());
+    }
+
+    private void testTenantNotFound(String tenantId) {
+        given()
+        .when().get(TENANTS_PATH + "/" + tenantId)
+        .then()
+           .statusCode(404);
+    }
+
+    public static String toString(List<TenantResource> resources) {
+        StringBuilder builder = new StringBuilder();
+        if (resources != null && !resources.isEmpty()) {
+            List<TenantResource> sorted = new ArrayList<>(resources);
+            sorted.sort((r1, r2) -> r1.getType().compareTo(r2.getType()));
+            sorted.forEach(r -> {
+                builder.append(r.getType());
+                builder.append("@");
+                builder.append(r.getLimit());
+                builder.append("\n");
+            });
+        }
+        return builder.toString();
+    }
+
+}
---
+++ datamodel/src/main/java/io/apicurio/tenantmanager/api/TenantsResource.java
@@ -0,0 +1,67 @@
+package io.apicurio.tenantmanager.api;
+
+import io.apicurio.tenantmanager.api.datamodel.NewApicurioTenantRequest;
+import io.apicurio.tenantmanager.api.datamodel.ApicurioTenant;
+import io.apicurio.tenantmanager.api.datamodel.ApicurioTenantList;
+import io.apicurio.tenantmanager.api.datamodel.SortBy;
+import io.apicurio.tenantmanager.api.datamodel.SortOrder;
+import io.apicurio.tenantmanager.api.datamodel.UpdateApicurioTenantRequest;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+
+/**
+ * A JAX-RS interface.  An implementation of this interface must be provided.
+ */
+@Path("/api/v1/tenants")
+public interface TenantsResource {
+  /**
+   * Gets the details of a single instance of a `Tenant`.
+   */
+  @Path("/{tenantId}")
+  @GET
+  @Produces("application/json")
+  ApicurioTenant getTenant(@PathParam("tenantId") String tenantId);
+
+  /**
+   * Updates the name, description, and resources for a tenant.
+   */
+  @Path("/{tenantId}")
+  @PUT
+  @Consumes("application/json")
+  void updateTenant(@PathParam("tenantId") String tenantId, UpdateApicurioTenantRequest data);
+
+  /**
+   * Marks an existing `Tenant` to be deleted.
+   */
+  @Path("/{tenantId}")
+  @DELETE
+  void deleteTenant(@PathParam("tenantId") String tenantId);
+
+  /**
+   * Gets a list of `ApicurioTenant` entities according to the query parameters set.
+   */
+  @GET
+  @Produces("application/json")
+  ApicurioTenantList getTenants(@QueryParam("status") String status,
+      @QueryParam("offset") @Min(0) Integer offset, @QueryParam("limit") @Min(1) @Max(500) Integer limit,
+      @QueryParam("order") SortOrder order, @QueryParam("orderby") SortBy orderby);
+
+  /**
+   * Creates a new instance of a `Tenant`.
+   */
+  @POST
+  @Produces("application/json")
+  @Consumes("application/json")
+  Response createTenant(NewApicurioTenantRequest data);
+}
