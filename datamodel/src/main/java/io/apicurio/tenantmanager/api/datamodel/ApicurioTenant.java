11740419eaf1e5a89daa6d0dd7afc22293ea0fd1
Andrea Peruffo <andrea.peruffo1982@gmail.com>
2022-09-26 14:10:32 +0100

    Fix createdOn date format

--- datamodel/src/main/java/io/apicurio/tenantmanager/api/datamodel/ApicurioTenant.java
+++ datamodel/src/main/java/io/apicurio/tenantmanager/api/datamodel/ApicurioTenant.java
@@ -45,7 +45,7 @@ public class ApicurioTenant {
      * (Required)
      * 
      */
-    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ssZ", timezone = "UTC")
     @JsonProperty("createdOn")
     @JsonPropertyDescription("Date when the tenant was created. ISO 8601 UTC timestamp.")
     private Date createdOn;
